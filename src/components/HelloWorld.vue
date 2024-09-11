072d5626b095af4d785f1fbfc2ad91ddc4dc4f77
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-09-11 18:18:43 +0800

    Add scadcn button and update example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 
+import { Button } from '~/components/ui/button'
+
 defineProps({
   msg: String,
 })
@@ -9,35 +11,34 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="container">
+    <h1  class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+    <div class="mt-6">
+      <Button @click="count++">count is {{ count }}</Button>
+
+      <p class="mt-2">
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
+    <p class="mt-4">
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+        >create-vue</a
+      >, the official Vue + Vite starter
     </p>
-  </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p class="mt-2">
+      Learn more about IDE Support for Vue in the
+      <a
+        href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+        target="_blank"
+        >Vue Docs Scaling up Guide</a
+      >.
+    </p>
+    <p class="text-sm text-muted-foreground">Click on the Vite and Vue logos to learn more</p>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
