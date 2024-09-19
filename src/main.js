65edc82fffdf85fa6de9c160095a36c5bc26cdf4
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-09-19 10:08:45 +0800

    refactor: rename css file to app.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './styles/index.css'
+import './styles/app.css'
 
 const app = createApp(App)
 
