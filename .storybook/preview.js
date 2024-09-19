9fc6631dbc220dd58f8cef3249ed78e6816171f5
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-09-19 10:44:58 +0800

    Add @storybook/addon-themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { withThemeByClassName } from '@storybook/addon-themes'
+import '../src/styles/app.css'
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
@@ -13,7 +16,35 @@ const preview = {
         locales: 'en-US',
       },
     },
+    themes: {
+      target: 'html',
+    },
+    backgrounds: { disable: true },
   },
+
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+
+    // Decorator to apply bg-color to stories in dark mode
+    (_) => {
+      const body = document.querySelector('body')
+      const stories = document.querySelectorAll('.docs-story')
+      const THEME_CLASS = 'bg-background'
+
+      body?.classList.add(THEME_CLASS)
+      stories.forEach((element) => {
+        element.classList.add(THEME_CLASS)
+      })
+
+      return { template: `<story/>` }
+    },
+  ],
 }
 
 export default preview
