19592863a79ea8dfc44217a9558d1633dadbb15b
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-09-23 10:25:04 +0800

    fix(storybook): hide default story, prevent first story to be shown twice
    - https://github.com/storybookjs/storybook/discussions/24700
    - https://storybook.js.org/docs/writing-docs/autodocs#write-a-custom-template

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,15 +1,25 @@
 import { withThemeByClassName } from '@storybook/addon-themes'
+
+// eslint-disable-next-line no-unused-vars
+import { Controls, Description, Primary, Stories, Subtitle, Title } from '@storybook/blocks'
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
 import '../src/styles/app.css'
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
+    backgrounds: { disable: true },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+
     options: {
       storySort: {
         order: [
@@ -20,10 +30,23 @@ const preview = {
         locales: 'en-US',
       },
     },
+
     themes: {
       target: 'html',
     },
-    backgrounds: { disable: true },
+
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+          <Stories includePrimary={false} />
+        </>
+      ),
+    },
   },
 
   decorators: [
