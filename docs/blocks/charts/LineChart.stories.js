c4e09ccf5a60a6a1805469c94c9b5dc2d7568d25
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-10-03 11:41:36 +0800

    Add line chart story

---
+++ docs/blocks/charts/LineChart.stories.js
@@ -0,0 +1,73 @@
+import { LineChart } from '~/components/ui/chart-line'
+import data from './data/line-chart-01.json'
+
+export default {
+  title: 'Blocks/Charts/Line Chart',
+  component: LineChart,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      source: {
+        /* eslint-disable no-template-curly-in-string */
+        code: `
+<script setup>
+const data = [
+   {
+    'year': 1970,
+    'Export Growth Rate': 2.04,
+    'Import Growth Rate': 1.53,
+  },
+  // ...
+]
+</script>
+
+<template>
+  <LineChart
+    :data="data"
+    index="year"
+    :categories="['Export Growth Rate', 'Import Growth Rate']"
+    :y-formatter="(tick, _i) => {
+      return typeof tick === 'number'
+          ? ` + '$ ${new Intl.NumberFormat(\'us\').format(tick).toString()}' + `
+        : ''
+    }"
+  />
+</template>
+`,
+      /* eslint-enable no-template-curly-in-string */
+      },
+    },
+  },
+}
+
+export const Default = {
+  args: {
+    data,
+    'index': 'year',
+    'categories': ['Export Growth Rate', 'Import Growth Rate'],
+    'y-formatter': (tick, _i) => {
+      return typeof tick === 'number'
+        ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+        : ''
+    },
+  },
+}
+
+export const Sparkline = {
+  args: {
+    data,
+    'class': 'h-[100px] w-[400px]',
+    'index': 'year',
+    'categories': ['Export Growth Rate'],
+    'y-formatter': (tick, _i) => {
+      return typeof tick === 'number'
+        ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+        : ''
+    },
+    'show-tooltip': false,
+    'show-grid-line': false,
+    'show-legend': false,
+    'show-x-axis': false,
+    'show-y-axis': false,
+  },
+}
