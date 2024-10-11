65d3a9ff2bcf2985089226d69a4620617455e5ce
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-10-11 14:50:00 +0800

    Add VCalendar story

---
+++ docs/components/VCalendar/Default.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { VCalendar } from '~/components/ui/v-calendar'
+
+const date = ref(new Date())
+</script>
+
+<template>
+  <VCalendar v-model="date" class="rounded-md border" />
+</template>
---
+++ docs/components/VCalendar/VCalendar.stories.js
@@ -0,0 +1,39 @@
+import DefaultComponent from './Default.vue'
+import DefaultSource from './Default.vue?raw'
+
+/**
+ * The Calendar component is built on top of [VCalendar](https://vcalendar.io/getting-started/installation.html).
+ *
+ * Read more about VCalendar at https://vcalendar.io.<br>
+ * There is a risk that the project is abandoned. Read issue [#1503](https://github.com/nathanreyes/v-calendar/issues/1503)
+ */
+export default {
+  title: 'Form/VCalendar',
+  component: DefaultComponent,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultComponent },
+
+    setup() {
+      return { args }
+    },
+
+    // FIXME: the component fills the available space. We put some constraint
+    template: `
+      <div class="max-w-[278px]">
+        <DefaultComponent v-bind="args" />
+      </div>
+    `,
+  }),
+}
