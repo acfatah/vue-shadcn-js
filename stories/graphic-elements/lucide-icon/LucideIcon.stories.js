4133a5b16e993ec5a23c61694674dfdb84976f25
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-10-19 11:43:48 +0800

    refactor: improve lucide story props

--- stories/graphic-elements/lucide-icon/LucideIcon.stories.js
+++ stories/graphic-elements/lucide-icon/LucideIcon.stories.js
@@ -5,6 +5,9 @@ import ExampleSource from './Example.vue?raw'
 /**
  * Automatically import lucide icon as component. Simply postfix `Icon` to lucide icon name.
  *
+ * It's recommended to use `size-*` utility class to size the icon. The default
+ * size is `size-6` or  `24px`.
+ *
  * Browse Lucide icons at https://lucide.dev/icons.<br>
  * Read more about Lucide icons at https://lucide.dev/guide/packages/lucide-vue-next.
  */
@@ -12,6 +15,53 @@ export default {
   title: 'Graphic Elements/Lucide Icon',
   component: ExampleComponent,
   tags: ['autodocs'],
+
+  argTypes: {
+    size: {
+      control: 'number',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 24 },
+      },
+      description: 'Icon size',
+    },
+
+    color: {
+      control: 'text',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'currentColor' },
+      },
+      description: 'Icon color',
+    },
+
+    strokeWidth: {
+      control: 'number',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: 2 },
+      },
+      description: 'Icon stroke width',
+    },
+
+    absoluteStrokeWidth: {
+      control: 'boolean',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+      description: 'Absolute stroke width',
+    },
+
+    defaultClass: {
+      control: 'text',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'lucide-icon' },
+      },
+      description: 'Default class',
+    },
+  },
 }
 
 export const Default = {
@@ -23,14 +73,6 @@ export const Default = {
         code: ExampleSource,
       },
     },
-  },
-
-  args: {
-    size: 24,
-    color: 'currentColor',
-    strokeWidth: 2,
-    absoluteStrokeWidth: false,
-    defaultClass: 'lucide-icon',
   },
 
   render: args => ({
