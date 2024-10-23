1bbd88d69b6ed4b987e6036935592abc3e3f0666
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-10-23 10:00:20 +0800

    fix: responsive shadcn form example

--- stories/shadcn/examples/forms/components/SidebarNav.vue
+++ stories/shadcn/examples/forms/components/SidebarNav.vue
@@ -3,14 +3,14 @@ import routes from '../routes.js'
 </script>
 
 <template>
-  <nav class="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
+  <nav class="flex flex-wrap space-y-1 sm:flex-nowrap sm:gap-x-1 sm:space-y-0 lg:flex-col lg:space-y-1">
     <Button
       v-for="item in routes"
       :key="item.name"
       :href="item.path"
       variant="ghost"
       :class="cn(
-        'w-full text-left justify-start',
+        'w-full justify-start px-2 text-left md:px-4',
         $route.path === item.path && 'bg-muted hover:bg-muted',
       )"
       @click.stop.prevent="$router.push(item.path)"
--- stories/shadcn/examples/forms/layouts/FormsLayout.vue
+++ stories/shadcn/examples/forms/layouts/FormsLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import SidebarNav from '../components/SidebarNav.vue'
+import Title from './Title.vue'
 
 const router = useRouter()
 
@@ -9,19 +10,13 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="md:hidden">
-    TBD: Responsive mobile layout
+  <div class="flex min-h-screen w-full flex-col space-y-6 p-4 md:hidden">
+    <Title />
+    <SidebarNav />
+    <RouterView />
   </div>
   <div class="hidden space-y-6 p-10 pb-16 md:block">
-    <div class="space-y-0.5">
-      <h2 class="text-2xl font-bold tracking-tight">
-        Settings
-      </h2>
-      <p class="text-muted-foreground">
-        Manage your account settings and set e-mail preferences.
-      </p>
-    </div>
-    <Separator class="my-6" />
+    <Title />
     <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
       <aside class="-mx-4 lg:w-1/5">
         <SidebarNav />
