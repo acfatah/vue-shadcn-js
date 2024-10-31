439e40014a17139128feaa09c17d5256be0da04e
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-10-31 09:53:20 +0800

    fix: missing sidebar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -89,8 +89,14 @@ module.exports = {
           foreground: 'hsl(var(--card-foreground))',
         },
         sidebar: {
-          DEFAULT: 'hsl(var(--sidebar))',
-          foreground: 'hsl(var(--sidebar-foreground))',
+          'DEFAULT': 'hsl(var(--sidebar))',
+          'foreground': 'hsl(var(--sidebar-foreground))',
+          'primary': 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          'accent': 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          'border': 'hsl(var(--sidebar-border))',
+          'ring': 'hsl(var(--sidebar-ring))',
         },
       },
 
