8cefcf80c05884d75f417149117d7444d327ed98
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-11-12 12:18:28 +0800

    Add TagsInput with combobox story and refactor story file naming

--- stories/forms/TagsInput/TagsInput.stories.js
+++ stories/forms/TagsInput/TagsInput.stories.js
@@ -5,8 +5,9 @@ import {
   TagsInputItemDelete,
   TagsInputItemText,
 } from '~/components/ui/tags-input'
-import DefaultComponent from './Default.vue'
-import DefaultSource from './Default.vue?raw'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import WithComboboxComponent from './WithCombobox.vue'
 
 /**
  * Tag inputs render tags inside an input, followed by an actual text input.
@@ -15,7 +16,7 @@ import DefaultSource from './Default.vue?raw'
  */
 export default {
   title: 'Forms/Tags Input',
-  component: DefaultComponent,
+  component: DefaultStory,
   subcomponents: {
     TagsInput,
     TagsInputInput,
@@ -36,14 +37,28 @@ export const Default = {
   },
 
   render: args => ({
-    components: { DefaultComponent },
+    components: { DefaultStory },
 
     setup() {
       return { args }
     },
 
     template: `
-      <DefaultComponent v-bind="args" />
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const WithCombobox = {
+  render: args => ({
+    components: { WithComboboxComponent },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <WithComboboxComponent v-bind="args" />
     `,
   }),
 }
---
+++ stories/forms/TagsInput/WithCombobox.vue
@@ -0,0 +1,62 @@
+<script setup>
+const frameworks = [
+  { value: 'next.js', label: 'Next.js' },
+  { value: 'sveltekit', label: 'SvelteKit' },
+  { value: 'nuxt', label: 'Nuxt' },
+  { value: 'remix', label: 'Remix' },
+  { value: 'astro', label: 'Astro' },
+]
+
+const modelValue = ref([])
+const open = ref(false)
+const searchTerm = ref('')
+
+const filteredFrameworks = computed(() => frameworks.filter(i => !modelValue.value.includes(i.label)))
+</script>
+
+<template>
+  <TagsInput class="w-80 gap-0 px-0" :model-value="modelValue">
+    <div class="flex flex-wrap items-center gap-2 px-3">
+      <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
+        <TagsInputItemText />
+        <TagsInputItemDelete />
+      </TagsInputItem>
+    </div>
+
+    <ComboboxRoot v-model="modelValue" v-model:open="open" v-model:search-term="searchTerm" class="w-full">
+      <ComboboxAnchor as-child>
+        <ComboboxInput placeholder="Framework..." as-child>
+          <TagsInputInput class="w-full px-3" :class="modelValue.length > 0 ? 'mt-2' : ''" @keydown.enter.prevent />
+        </ComboboxInput>
+      </ComboboxAnchor>
+
+      <ComboboxPortal>
+        <ComboboxContent>
+          <CommandList
+            position="popper"
+            class="mt-2 w-[--radix-popper-anchor-width] rounded-md border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+          >
+            <CommandEmpty />
+            <CommandGroup>
+              <CommandItem
+                v-for="framework in filteredFrameworks" :key="framework.value" :value="framework.label"
+                @select.prevent="(ev) => {
+                  if (typeof ev.detail.value === 'string') {
+                    searchTerm = ''
+                    modelValue.push(ev.detail.value)
+                  }
+
+                  if (filteredFrameworks.length === 0) {
+                    open = false
+                  }
+                }"
+              >
+                {{ framework.label }}
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </ComboboxContent>
+      </ComboboxPortal>
+    </ComboboxRoot>
+  </TagsInput>
+</template>
