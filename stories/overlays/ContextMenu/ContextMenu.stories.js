bb7db5a966771145baeed62e6bfb87b3b1375b62
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-11-13 17:17:46 +0800

    Add ContextMenu story

---
+++ stories/overlays/ContextMenu/ContextMenu.stories.js
@@ -0,0 +1,35 @@
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Displays a menu to the user — such as a set of actions or functions — triggered by a button.
+ *
+ * Primitive API Reference: https://www.radix-vue.com/components/context-menu.html
+ */
+export default {
+  title: 'Overlays/ContextMenu',
+  component: DefaultStory,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
