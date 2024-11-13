8c5004c6a1b56f64b09e50a6a1ea2e841a15da0f
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-11-13 11:02:13 +0800

    Add more Carousel stories

--- stories/graphic-elements/Carousel/Carousel.stories.js
+++ stories/graphic-elements/Carousel/Carousel.stories.js
@@ -1,8 +1,32 @@
 import DefaultStory from './DefaultStory.vue'
 import DefaultSource from './DefaultStory.vue?raw'
+import ThumbnailsStory from './ThumbnailsStory.vue'
+import ThumbnailsSource from './ThumbnailsStory.vue?raw'
+import VerticalStory from './VerticalStory.vue'
+import VerticalSource from './VerticalStory.vue?raw'
 
 /**
  * A carousel with motion and swipe built using Embla.
+ *
+ * ## Sizes
+ *
+ * Sizes can be set using the basis utility class on the `<CarouselItem />`.
+ *
+ * ## Spacing
+ *
+ * To set the spacing between the items, we use a `pl-[VALUE]` utility on the
+ * <CarouselItem /> and a negative `-ml-[VALUE]` on the `<CarouselContent />`.
+ *
+ * ## Options
+ *
+ * Pass options to the carousel using the `opts` prop.<br>
+ * See: https://www.embla-carousel.com/api/options
+ *
+ * ## Plugins
+ *
+ * See: https://www.embla-carousel.com/plugins
+ *
+ * ## References
  *
  * Primitive API Reference: https://www.embla-carousel.com/api
  */
@@ -29,8 +53,56 @@ export const Default = {
     },
 
     template: `
-      <div class="flex w-full justify-center">
+      <div class="flex w-full justify-center p-10">
         <DefaultStory v-bind="args" />
+      </div>
+    `,
+  }),
+}
+
+export const Vertical = {
+  parameters: {
+    docs: {
+      source: {
+        code: VerticalSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { VerticalStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <div class="flex w-full h-full items-center justify-center min-h-[350px] p-10">
+        <VerticalStory v-bind="args" />
+      </div>
+    `,
+  }),
+}
+
+export const Thumbnails = {
+  parameters: {
+    docs: {
+      source: {
+        code: ThumbnailsSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { ThumbnailsStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <div class="flex w-full h-full items-center justify-center min-h-[350px] p-10">
+        <ThumbnailsStory v-bind="args" />
       </div>
     `,
   }),
---
+++ stories/graphic-elements/Carousel/ThumbnailsStory.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { watchOnce } from '@vueuse/core'
+
+const emblaMainApi = ref(null)
+const emblaThumbnailApi = ref(null)
+const selectedIndex = ref(0)
+
+function onSelect() {
+  if (!emblaMainApi.value || !emblaThumbnailApi.value)
+    return
+  selectedIndex.value = emblaMainApi.value.selectedScrollSnap()
+  emblaThumbnailApi.value.scrollTo(emblaMainApi.value.selectedScrollSnap())
+}
+
+function onThumbClick(index) {
+  if (!emblaMainApi.value || !emblaThumbnailApi.value)
+    return
+  emblaMainApi.value.scrollTo(index)
+}
+
+watchOnce(emblaMainApi, (emblaMainApi) => {
+  if (!emblaMainApi)
+    return
+
+  onSelect()
+  emblaMainApi.on('select', onSelect)
+  emblaMainApi.on('reInit', onSelect)
+})
+</script>
+
+<template>
+  <div class="w-full sm:w-auto">
+    <Carousel
+      class="relative w-full max-w-xs"
+      @init-api="(val) => emblaMainApi = val"
+    >
+      <CarouselContent>
+        <CarouselItem v-for="(_, index) in 10" :key="index">
+          <div class="p-1">
+            <Card>
+              <CardContent class="flex aspect-square items-center justify-center p-6">
+                <span class="text-4xl font-semibold">{{ index + 1 }}</span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+
+    <Carousel
+      class="relative w-full max-w-xs"
+      @init-api="(val) => emblaThumbnailApi = val"
+    >
+      <CarouselContent class="ml-0 flex gap-1">
+        <CarouselItem v-for="(_, index) in 10" :key="index" class="basis-1/4 cursor-pointer pl-0" @click="onThumbClick(index)">
+          <div class="p-1" :class="index === selectedIndex ? '' : 'opacity-50'">
+            <Card>
+              <CardContent class="flex aspect-square items-center justify-center p-6">
+                <span class="text-4xl font-semibold">{{ index + 1 }}</span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  </div>
+</template>
