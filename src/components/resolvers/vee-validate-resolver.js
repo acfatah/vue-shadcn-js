bb38de91e336dda9fccc519b3899903ae3a8d3d2
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-11-18 11:11:05 +0800

    refactor: vee-validate auto import

--- src/components/resolvers/vee-validate-resolver.js
+++ src/components/resolvers/vee-validate-resolver.js
@@ -3,9 +3,10 @@
  */
 export default function (componentName) {
   const components = [
-    { name: 'Form', from: 'vee-validate' },
+    { name: 'ErrorMessage', from: 'vee-validate' },
     { name: 'Field', as: 'FormField', from: 'vee-validate' },
     { name: 'FieldArray', from: 'vee-validate' },
+    { name: 'Form', from: 'vee-validate' },
   ]
 
   return components.find(
