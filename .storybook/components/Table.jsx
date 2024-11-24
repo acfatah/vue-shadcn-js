d7b0b95cdb3666ea77a4e4430fb20a14e55a4f74
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-11-25 06:26:38 +0800

    Add spacing story

---
+++ .storybook/components/Table.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Table({ headers, rows }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {Object.keys(row).map((cell, cellIndex) => (
+              <>
+                <td key={cellIndex}>
+                  {row[cell]}
+                </td>
+              </>
+            ))}
+            <td>
+              <div
+                style={{
+                  minHeight: '24px',
+                  width: `${row.size}`,
+                  backgroundColor: 'green',
+                }}
+              >
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
