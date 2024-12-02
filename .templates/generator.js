fb66ae6f9759931c867ae597f32dd8fd8a5e354a
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-02 09:42:17 +0800

    Add story generator

--- .templates/generator.js
+++ .templates/generator.js
@@ -1,5 +1,9 @@
 import storeGenerator from './store/index.js'
+import storyGenerator from './stories/index.js'
 
 export default function (plop) {
+  plop.setHelper('orValues', (...values) => values.find(value => value))
+
   plop.setGenerator('store', storeGenerator)
+  plop.setGenerator('story', storyGenerator)
 }
---
+++ .templates/stories/index.js
@@ -0,0 +1,32 @@
+export const storyGenerator = {
+  description: 'component story',
+
+  prompts: [
+    {
+      type: 'input',
+      name: 'path',
+      message: 'component path, should end with /',
+      default: '',
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'component name in PascalCase',
+    },
+  ],
+
+  actions: [
+    {
+      type: 'add',
+      path: '../stories/{{orValues path name}}/{{name}}.stories.js',
+      templateFile: 'stories/files/Name.stories.js.hbs',
+    },
+    {
+      type: 'add',
+      path: '../stories/{{orValues path name}}/DefaultStory.vue',
+      templateFile: 'stories/files/DefaultStory.vue.hbs',
+    },
+  ],
+}
+
+export default storyGenerator
